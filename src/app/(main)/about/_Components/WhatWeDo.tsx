19d311e4e2d15d048ab2f6e3ef04d3f646eef247
towfiq-islam <tofiqislam03@gmail.com>
2025-12-20 17:20:07 +0600

    added

--- src/app/(main)/about/_Components/WhatWeDo.tsx
+++ src/app/(main)/about/_Components/WhatWeDo.tsx
@@ -1,10 +1,81 @@
-import React from 'react';
+import {
+  WFiveSvg,
+  WFourSvg,
+  WOneSvg,
+  WSixSvg,
+  WThreeSvg,
+  WTwoSvg,
+} from "@/Components/Svg/SvgContainer";
+
+const data = [
+  {
+    id: 1,
+    icon: <WOneSvg />,
+    title: "Business Spotlights",
+    description:
+      "Showcasing local entrepreneurs and helping them build visibility and credibility.",
+  },
+  {
+    id: 2,
+    icon: <WTwoSvg />,
+    title: "Artist & Creative Features",
+    description:
+      "Highlighting musicians, visual artists, designers, models, creators, and innovators.",
+  },
+  {
+    id: 3,
+    icon: <WThreeSvg />,
+    title: "Boss Beginnings: A Business Shower",
+    description: "A celebration and support system for new business owners.",
+  },
+  {
+    id: 4,
+    icon: <WFourSvg />,
+    title: "Cultural Storytelling",
+    description: "Documenting real stories, struggles, and triumphs.",
+  },
+  {
+    id: 5,
+    icon: <WFiveSvg />,
+    title: "Community Engagement & Events",
+    description:
+      "Interviews, events, collaborations, and creative opportunities.",
+  },
+  {
+    id: 6,
+    icon: <WSixSvg />,
+    title: "Sponsorship & Advertising",
+    description: "Affordable promotions and community-driven exposure.",
+  },
+];
 
 const WhatWeDo = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="container py-20">
+      <h2 className="text-primary-black text-5xl font-bold leading-[140%] text-center mb-12">
+        Mission & Purpose
+      </h2>
+
+      <div className="grid grid-cols-3 gap-5">
+        {data?.map(item => (
+          <div className="border border-[#00000013] rounded-xl px-5 py-10 shadow-[0_4px_20px_0_rgba(0,0,0,0.07)] flex flex-col gap-4 justify-between">
+            <div className="space-y-4">
+              <span className="size-16 grid place-items-center rounded-xl border border-[#00000013]">
+                {item?.icon}
+              </span>
+
+              <h3 className="font-medium text-2xl text-[#1D1D1F] max-w-[350px] leading-[150%]">
+                {item?.title}
+              </h3>
+            </div>
+
+            <p className="text-[#1D1D1F] text-xl leading-[150%]">
+              {item?.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
