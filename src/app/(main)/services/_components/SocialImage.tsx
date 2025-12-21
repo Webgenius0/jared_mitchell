8b42789e85d4778823bcf5932af48e0fe75d339a
towfiq-islam <tofiqislam03@gmail.com>
2025-12-21 18:02:13 +0600

    fixed issues

--- src/app/(main)/services/_components/SocialImage.tsx
+++ src/app/(main)/services/_components/SocialImage.tsx
@@ -1,31 +1,41 @@
-import { Button } from '@/Components/Common/Button'
-import Container from '@/Components/Common/Container'
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/Components/Common/Button";
+import Container from "@/Components/Common/Container";
+import Image from "next/image";
 
 const SocialImage = () => {
   return (
-    <section className='section'>
+    <section className="section">
       <Container>
-        <div className='flex max-lg:flex-col items-center gap-10 2xl:gap-20'>
-          <div className='flex-1'>
-            <h2 className='section_title text-left'>
+        <div className="flex max-lg:flex-col items-center gap-10 2xl:gap-20">
+          <div className="flex-1">
+            <h2 className="section_title !text-left">
               Grow With Our Social Image
             </h2>
-            <p className='text-2xl text-secondary-black mt-7 mb-9'>
-              Our Social Image isn't just a platform — it's an ecosystem designed to amplify your voice, elevate your work, and help you grow your brand with purpose.
-              You'll gain access to tools, visibility channels, community support, and AI-driven insights that help you understand your audience, improve your image, and reach real people who want to support your journey.
-              Whether you're an artist, entrepreneur, student, business owner, or creator — OSI is built to help you thrive.
+            <p className="text-2xl text-secondary-black mt-7 mb-9">
+              Our Social Image isn't just a platform — it's an ecosystem
+              designed to amplify your voice, elevate your work, and help you
+              grow your brand with purpose. You'll gain access to tools,
+              visibility channels, community support, and AI-driven insights
+              that help you understand your audience, improve your image, and
+              reach real people who want to support your journey. Whether you're
+              an artist, entrepreneur, student, business owner, or creator — OSI
+              is built to help you thrive.
             </p>
             <Button>Join the Movement</Button>
           </div>
-          <figure className='max-w-[665px] lg:w-1/2 2xl:w-full h-[616px] rounded-2xl md:rounded-3xl overflow-hidden'>
-            <Image src={'/services/social-image.jpg'} width={665} height={616} alt='' className='size-full object-cover'/>
+          <figure className="max-w-[665px] lg:w-1/2 2xl:w-full h-[616px] rounded-2xl md:rounded-3xl overflow-hidden">
+            <Image
+              src={"/services/social-image.jpg"}
+              width={665}
+              height={616}
+              alt=""
+              className="size-full object-cover"
+            />
           </figure>
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default SocialImage
+export default SocialImage;
