137d58e97f13075e458d543f7ce11e604805c5c7
towfiq-islam <tofiqislam03@gmail.com>
2025-12-23 12:41:42 +0600

    added upcomming event page

---
+++ src/app/(main)/events/_Components/EventSchedule.tsx
@@ -0,0 +1,13 @@
+const EventSchedule = () => {
+  return (
+    <section className="py-20 container">
+      <h2 className="section_title 2xl:text-7xl 2xl:font-bold">
+        Upcoming Events
+      </h2>
+
+      
+    </section>
+  );
+};
+
+export default EventSchedule;
--- src/app/(main)/events/page.tsx
+++ src/app/(main)/events/page.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import EventsBanner from "./_Components/EventsBanner";
+import CreatorMarket from "./_Components/CreatorMarket";
+import FilterSection from "./_Components/FilterSection";
+import UpcomingEvents from "./_Components/UpcomingEvents";
+import EventSchedule from "./_Components/EventSchedule";
 
 const page = () => {
   return (
-    <div>
-      Events
-    </div>
+    <>
+      <EventsBanner />
+      <CreatorMarket />
+      <FilterSection />
+      <UpcomingEvents />
+      <EventSchedule />
+    </>
   );
 };
 
