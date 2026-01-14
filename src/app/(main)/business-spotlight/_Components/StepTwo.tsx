414203236b7e511dbba3c6d5c24f5fc6373158f6
towfiq-islam <tofiqislam03@gmail.com>
2026-01-14 18:04:25 +0600

    working in step form

--- src/app/(main)/business-spotlight/_Components/StepTwo.tsx
+++ src/app/(main)/business-spotlight/_Components/StepTwo.tsx
@@ -8,188 +8,141 @@ const StepTwo = () => {
 
   return (
     <div className="step_box">
-      <h2 className="text-3xl font-semibold mb-2">Business Information</h2>
+      <h2 className="text-3xl font-semibold mb-2">Business Story</h2>
       <p className="text-[#364153] text-xl mb-5">
-        Tell us the basics about your business
+        Help us understand your journey and what makes you special
       </p>
       <hr className="text-gray-200 mb-10 block" />
 
       <div className="grid grid-cols-2 gap-7">
-        {/* Business name */}
+        {/* Business Story */}
         <div className="col-span-2">
           <p className="flex justify-between items-center mb-2">
-            <label htmlFor="business_name" className="auth_label">
-              Business Name <span>*</span>
+            <label htmlFor="business_story" className="auth_label">
+              1. Tell us your business story <span>*</span>
             </label>
-            {errors.business_name?.message && (
+            {errors.business_story?.message && (
               <p className="text-red-500">
-                {errors.business_name.message as string}
+                {errors.business_story.message as string}
               </p>
             )}
           </p>
 
-          <input
-            type="text"
-            id="business_name"
-            className={`step_input ${
-              errors.business_name ? "border-red-500" : "border-[#D1D5DC] "
+          <textarea
+            id="business_story"
+            rows={5}
+            className={`step_input !rounded-xl ${
+              errors.business_story ? "border-red-500" : "border-[#D1D5DC] "
             }`}
-            placeholder="Enter your business name"
-            {...register("business_name", {
-              required: "Business Name is required",
+            placeholder="Where it started, why it exists, and the mission behind it..."
+            {...register("business_story", {
+              required: "Business Story is required",
             })}
-          />
+          ></textarea>
         </div>
 
-        {/* Owner Name */}
+        {/* Product Offer */}
         <div className="col-span-2">
           <p className="flex justify-between items-center mb-2">
-            <label htmlFor="owner_name" className="auth_label">
-              Owner / Founder Name <span>*</span>
+            <label htmlFor="product_offer" className="auth_label">
+              2. What products or services do you offer? <span>*</span>
             </label>
-            {errors.owner_name?.message && (
+            {errors.product_offer?.message && (
               <p className="text-red-500">
-                {errors.owner_name.message as string}
+                {errors.product_offer.message as string}
               </p>
             )}
           </p>
-          <input
-            type="text"
-            id="owner_name"
-            className={`step_input ${
-              errors.business_name ? "border-red-500" : "border-[#D1D5DC] "
+
+          <textarea
+            id="product_offer"
+            rows={5}
+            className={`step_input !rounded-xl ${
+              errors.product_offer ? "border-red-500" : "border-[#D1D5DC] "
             }`}
-            placeholder="Your name"
-            {...register("owner_name", {
-              required: "Owner Name is required",
+            placeholder="List what customers can purchase or experience..."
+            {...register("product_offer", {
+              required: "Product Offer is required",
             })}
-          />
+          ></textarea>
         </div>
 
-        {/* Business Category */}
+        {/* Business Outcome */}
         <div className="col-span-2">
           <p className="flex justify-between items-center mb-2">
-            <label htmlFor="business_category" className="auth_label">
-              Business Category <span>*</span>
+            <label htmlFor="business_outcome" className="auth_label">
+              3. What challenges has your business overcome? <span>*</span>
             </label>
-            {errors.business_category?.message && (
+            {errors.business_outcome?.message && (
               <p className="text-red-500">
-                {errors.business_category.message as string}
+                {errors.business_outcome.message as string}
               </p>
             )}
           </p>
-          <select
-            id="business_category"
-            className={`step_input ${
-              errors.business_category ? "border-red-500" : "border-[#D1D5DC] "
+
+          <textarea
+            id="business_outcome"
+            rows={5}
+            className={`step_input !rounded-xl ${
+              errors.business_outcome ? "border-red-500" : "border-[#D1D5DC] "
             }`}
-            {...register("business_category", {
-              required: "Business category is required",
+            placeholder="Share the milestones, struggles, or turning points..."
+            {...register("business_outcome", {
+              required: "Business outcome is required",
             })}
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            <option value="1">Category 1</option>
-            <option value="2">Category 2</option>
-            <option value="3">Category 3</option>
-          </select>
+          ></textarea>
         </div>
 
-        {/* Year Foundation */}
-        <div>
+        {/* Business Unique */}
+        <div className="col-span-2">
           <p className="flex justify-between items-center mb-2">
-            <label htmlFor="founded_year" className="auth_label">
-              Year Founded <span>*</span>
+            <label htmlFor="business_unique" className="auth_label">
+              4. What makes your business unique?<span>*</span>
             </label>
-            {errors.founded_year?.message && (
+            {errors.business_unique?.message && (
               <p className="text-red-500">
-                {errors.founded_year.message as string}
+                {errors.business_unique.message as string}
               </p>
             )}
           </p>
-          <input
-            type="text"
-            id="founded_year"
-            className={`step_input ${
-              errors.business_name ? "border-red-500" : "border-[#D1D5DC] "
+
+          <textarea
+            id="business_unique"
+            rows={5}
+            className={`step_input !rounded-xl ${
+              errors.business_unique ? "border-red-500" : "border-[#D1D5DC] "
             }`}
-            placeholder="2020"
-            {...register("founded_year", {
-              required: "Founded year is required",
+            placeholder="What separates you from competitors..."
+            {...register("business_unique", {
+              required: "Business unique is required",
             })}
-          />
+          ></textarea>
         </div>
 
-        {/* Business Website */}
-        <div>
+        {/* Target Customer */}
+        <div className="col-span-2">
           <p className="flex justify-between items-center mb-2">
-            <label htmlFor="business_website" className="auth_label">
-              Business Website <span>*</span>
+            <label htmlFor="target_customer" className="auth_label">
+              5. Who is your target customer?<span>*</span>
             </label>
-            {errors.business_website?.message && (
+            {errors.target_customer?.message && (
               <p className="text-red-500">
-                {errors.business_website.message as string}
+                {errors.target_customer.message as string}
               </p>
             )}
           </p>
-          <input
-            type="text"
-            id="business_website"
-            className={`step_input ${
-              errors.business_name ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="https://yourbusiness.com"
-            {...register("business_website", {
-              required: "Business website is required",
-            })}
-          />
-        </div>
 
-        {/* City */}
-        <div>
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="city" className="auth_label">
-              City <span>*</span>
-            </label>
-            {errors.city?.message && (
-              <p className="text-red-500">{errors.city.message as string}</p>
-            )}
-          </p>
-          <input
-            type="text"
-            id="city"
-            className={`step_input ${
-              errors.business_name ? "border-red-500" : "border-[#D1D5DC] "
+          <textarea
+            id="target_customer"
+            rows={5}
+            className={`step_input !rounded-xl ${
+              errors.target_customer ? "border-red-500" : "border-[#D1D5DC] "
             }`}
-            placeholder="https://yourbusiness.com"
-            {...register("city", {
-              required: "City is required",
+            placeholder="Describe their demographics, needs, or interests..."
+            {...register("target_customer", {
+              required: "Target customer is required",
             })}
-          />
-        </div>
-
-        {/* State */}
-        <div>
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="state" className="auth_label">
-              State <span>*</span>
-            </label>
-            {errors.state?.message && (
-              <p className="text-red-500">{errors.state.message as string}</p>
-            )}
-          </p>
-          <input
-            type="text"
-            id="state"
-            className={`step_input ${
-              errors.business_name ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="https://yourbusiness.com"
-            {...register("state", {
-              required: "State is required",
-            })}
-          />
+          ></textarea>
         </div>
       </div>
     </div>
