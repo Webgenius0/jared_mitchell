faa91c42d55ebd23ef967898633e6cb4cd99d8d7
towfiq-islam <tofiqislam03@gmail.com>
2026-01-17 17:21:22 +0600

    working in artist spotlight

--- src/app/(main)/artist-spotlight/_Components/StepTwo.tsx
+++ src/app/(main)/artist-spotlight/_Components/StepTwo.tsx
@@ -1,151 +1,95 @@
+import { SelectSvg } from "@/Components/Svg/SvgContainer";
 import { useFormContext } from "react-hook-form";
+
+const data = [
+  { title: "Musician", desc: "Rap / R&B / Hip-Hop / Pop / Indie / Other" },
+  { title: "Visual Artist", desc: "Painter / Sketch / Mixed Media / etc." },
+  { title: "Photographer" },
+  { title: "Model" },
+  { title: "Videographer" },
+  { title: "Digital Creator" },
+  { title: "Designer", desc: "Fashion / Graphic" },
+  { title: "Poet / Writer" },
+  { title: "Other", desc: "Please describe below" },
+];
 
 const StepTwo = () => {
   const {
     register,
+    watch,
     formState: { errors },
   } = useFormContext();
 
+  const selectedCategory = watch("artistCategory");
+
   return (
     <div className="step_box">
-      <h2 className="text-3xl font-semibold mb-2">Business Story</h2>
+      <div className="flex gap-2 text-primary-black">
+        <p className="size-10 rounded-full grid place-items-center bg-[#EFF6FF] text-2xl text-primary-blue">
+          2
+        </p>
+        <h2 className="text-3xl font-semibold mb-2">Artist Category</h2>
+      </div>
+
       <p className="text-[#364153] text-xl mb-5">
-        Help us understand your journey and what makes you special
+        Select your primary category. This determines your voting pool and
+        spotlight placement.
       </p>
+
       <hr className="text-gray-200 mb-10 block" />
 
-      <div className="grid grid-cols-2 gap-7">
-        {/* Business Story */}
-        <div className="col-span-2">
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="business_story" className="auth_label">
-              1. Tell us your business story <span>*</span>
-            </label>
-            {errors.business_story?.message && (
-              <p className="text-red-500">
-                {errors.business_story.message as string}
-              </p>
-            )}
-          </p>
-
-          <textarea
-            id="business_story"
-            rows={5}
-            className={`step_input !rounded-xl ${
-              errors.business_story ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="Where it started, why it exists, and the mission behind it..."
-            {...register("business_story", {
-              required: "Business Story is required",
-            })}
-          ></textarea>
+      <div className="mb-5 mt-10 col-span-2">
+        <div className="flex gap-3 items-center mb-1">
+          <SelectSvg />
+          <h2 className="text-[22px]">Select Your Category</h2>
         </div>
 
-        {/* Product Offer */}
-        <div className="col-span-2">
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="product_offer" className="auth_label">
-              2. What products or services do you offer? <span>*</span>
-            </label>
-            {errors.product_offer?.message && (
-              <p className="text-red-500">
-                {errors.product_offer.message as string}
-              </p>
-            )}
-          </p>
-
-          <textarea
-            id="product_offer"
-            rows={5}
-            className={`step_input !rounded-xl ${
-              errors.product_offer ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="List what customers can purchase or experience..."
-            {...register("product_offer", {
-              required: "Product Offer is required",
-            })}
-          ></textarea>
-        </div>
-
-        {/* Business Outcome */}
-        <div className="col-span-2">
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="business_outcome" className="auth_label">
-              3. What challenges has your business overcome? <span>*</span>
-            </label>
-            {errors.business_outcome?.message && (
-              <p className="text-red-500">
-                {errors.business_outcome.message as string}
-              </p>
-            )}
-          </p>
-
-          <textarea
-            id="business_outcome"
-            rows={5}
-            className={`step_input !rounded-xl ${
-              errors.business_outcome ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="Share the milestones, struggles, or turning points..."
-            {...register("business_outcome", {
-              required: "Business outcome is required",
-            })}
-          ></textarea>
-        </div>
-
-        {/* Business Unique */}
-        <div className="col-span-2">
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="business_unique" className="auth_label">
-              4. What makes your business unique?<span>*</span>
-            </label>
-            {errors.business_unique?.message && (
-              <p className="text-red-500">
-                {errors.business_unique.message as string}
-              </p>
-            )}
-          </p>
-
-          <textarea
-            id="business_unique"
-            rows={5}
-            className={`step_input !rounded-xl ${
-              errors.business_unique ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="What separates you from competitors..."
-            {...register("business_unique", {
-              required: "Business unique is required",
-            })}
-          ></textarea>
-        </div>
-
-        
-        {/* Target Customer */}
-        <div className="col-span-2">
-          <p className="flex justify-between items-center mb-2">
-            <label htmlFor="target_customer" className="auth_label">
-              5. Who is your target customer?<span>*</span>
-            </label>
-            {errors.target_customer?.message && (
-              <p className="text-red-500">
-                {errors.target_customer.message as string}
-              </p>
-            )}
-          </p>
-
-          <textarea
-            id="target_customer"
-            rows={5}
-            className={`step_input !rounded-xl ${
-              errors.target_customer ? "border-red-500" : "border-[#D1D5DC] "
-            }`}
-            placeholder="Describe their demographics, needs, or interests..."
-            {...register("target_customer", {
-              required: "Target customer is required",
-            })}
-          ></textarea>
-        </div>
+        <p className="text-[#364153] text-xl">
+          At least one social media handle is required
+        </p>
       </div>
+
+      <div className="grid grid-cols-2 gap-5">
+        {data.map((item, idx) => {
+          const id = `artistCategory-${idx}`;
+          const isActive = selectedCategory === item?.title;
+
+          return (
+            <label
+              key={id}
+              htmlFor={id}
+              className={`border shadow py-3 px-4 rounded-lg flex flex-col gap-2 justify-center cursor-pointer transition-all min-h-[80px] hover:bg-[#EFF6FF] relative
+                ${
+                  isActive
+                    ? "border-primary-blue bg-[#EFF6FF]"
+                    : "border-gray-200 hover:border-primary-blue bg-white"
+                }`}
+            >
+              <h3 className="text-xl font-medium text-[#101828]">
+                {item.title}
+              </h3>
+
+              {item.desc && <p className="text-[#6A7282]">{item.desc}</p>}
+
+              <input
+                type="radio"
+                id={id}
+                value={item?.title}
+                className="absolute top-3 right-3"
+                {...register("artistCategory", {
+                  required: "Please select a category",
+                })}
+              />
+            </label>
+          );
+        })}
+      </div>
+
+      {errors.artistCategory && (
+        <p className="text-red-500 mt-4">
+          {errors.artistCategory.message as string}
+        </p>
+      )}
     </div>
   );
 };
