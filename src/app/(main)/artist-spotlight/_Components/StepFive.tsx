cdf0221939f947c0e5fea650eb3bf8f90e0618c5
towfiq-islam <tofiqislam03@gmail.com>
2026-01-18 14:27:39 +0600

    working in step form

--- src/app/(main)/artist-spotlight/_Components/StepFive.tsx
+++ src/app/(main)/artist-spotlight/_Components/StepFive.tsx
@@ -1,9 +1,107 @@
-import React from 'react';
+import { ImportantSvg } from "@/Components/Svg/SvgContainer";
+import { useFormContext } from "react-hook-form";
+
+const data = [
+  {
+    title: "Important Legal Information",
+    desc: "By checking the boxes below, you grant OSI (Our Spotlight Initiative) permission to feature your work and story across our platform, social media, and promotional materials. You retain all ownership rights to your original work.",
+  },
+  {
+    title: "Public Release Agreement:",
+    desc: "I agree that OSI can publish my photos, videos, story, interview, name, and likeness for spotlight features, social media posts, and promotional materials. I understand this content may be shared across multiple platforms.",
+  },
+  {
+    title: "Ownership Declaration",
+    desc: "I confirm that I own the rights to all submitted artwork, photos, videos, and content. I have not infringed on any copyrights, trademarks, or other intellectual property rights. I am legally authorized to grant OSI permission to use this content.",
+  },
+  {
+    title: "Interview Permission",
+    desc: "I agree to participate in video interviews and grant OSI permission to record, edit, and publish these interviews. I understand that interviews may be used for spotlight features, social media, and promotional content.",
+  },
+];
 
 const StepFive = () => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext();
+
+  const selectedCategory = watch("artistCategory");
+
   return (
-    <div>
-      Five
+    <div className="step_box">
+      <div className="flex gap-2 text-primary-black">
+        <p className="size-10 rounded-full grid place-items-center bg-[#EFF6FF] text-2xl text-primary-blue">
+          5
+        </p>
+        <h2 className="text-3xl font-semibold mb-2">Consent & Rights</h2>
+      </div>
+
+      <p className="text-[#364153] text-xl mb-10">
+        Legal protection and content permission. Please read carefully before
+        agreeing.
+      </p>
+
+      <div className="rounded-xl bg-[#F5F5F7] p-7">
+        <div className="mb-7">
+          <div className="flex gap-3 items-center mb-1">
+            <ImportantSvg />
+            <h2 className="text-[22px]">Important Legal Information</h2>
+          </div>
+
+          <p className="text-[#364153cb] text-xl">
+            By checking the boxes below, you grant OSI (Our Spotlight
+            Initiative) permission to feature your work and story across our
+            platform, social media, and promotional materials. You retain all
+            ownership rights to your original work.
+          </p>
+        </div>
+
+        <div className="space-y-5">
+          {data.map((item, idx) => {
+            const id = `artistCategory-${idx}`;
+            const isActive = selectedCategory === item?.title;
+
+            return (
+              <label
+                key={id}
+                htmlFor={id}
+                className={`border shadow py-3 px-4 rounded-lg flex flex-col gap-2 justify-center cursor-pointer transition-all min-h-[80px] hover:bg-[#EFF6FF]
+                ${
+                  isActive
+                    ? "border-primary-blue bg-[#EFF6FF]"
+                    : "border-gray-100 hover:border-primary-blue bg-white"
+                }`}
+              >
+                <div className="flex gap-2 items-center">
+                  <input
+                    type="radio"
+                    id={id}
+                    value={item?.title}
+                    className="size-4"
+                    {...register("artistCategory", {
+                      required: "Please select a category",
+                    })}
+                  />
+
+                  <h3 className="text-xl font-medium text-[#101828]">
+                    {item.title}
+                  </h3>
+                </div>
+
+                <p className="text-[#6A7282] text-lg">{item.desc}</p>
+              </label>
+            );
+          })}
+        </div>
+
+        {errors.artistCategory && (
+          <p className="text-red-500 mt-4">
+            {errors.artistCategory.message as string}
+          </p>
+        )}
+      </div>
     </div>
   );
 };
--- src/app/(main)/artist-spotlight/_Components/StepFour.tsx
+++ src/app/(main)/artist-spotlight/_Components/StepFour.tsx
@@ -1,9 +1,270 @@
-import React from 'react';
+import {
+  CheckSvg,
+  DownloadIconSvg,
+  ImgSvg,
+} from "@/Components/Svg/SvgContainer";
+import { useFormContext } from "react-hook-form";
 
 const StepFour = () => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext();
+
+  const ownerPhoto = watch("owner_portrait")?.[0];
+  const workspacePhoto = watch("workstation")?.[0];
+  const servicePhoto = watch("product_photos")?.[0];
+  const teamPhoto = watch("team_photo")?.[0];
+
   return (
-    <div>
-      Four
+    <div className="step_box">
+      <div className="flex gap-2 text-primary-black mb-2">
+        <p className="size-10 rounded-full grid place-items-center bg-[#EFF6FF] text-2xl text-primary-blue">
+          4
+        </p>
+        <h2 className="text-3xl font-semibold">Media Uploads</h2>
+      </div>
+
+      <p className="text-[#364153] text-xl mb-5">
+        Upload photos and videos for your spotlight card, video intro, and page
+        visuals. High-quality images make a better impression!
+      </p>
+
+      <div className="space-y-7">
+        {/* Professional Headshot / Portrait */}
+        <div>
+          <p className="artist_label mb-1">
+            <ImgSvg />
+            <span>
+              Professional Headshot / Portrait <span>*</span>
+            </span>
+          </p>
+
+          <div className="flex justify-between items-center mb-3">
+            <p className="text-[#364153]">
+              A clear, professional photo of you. This will be your main
+              spotlight image.
+            </p>
+
+            {errors.owner_portrait?.message && (
+              <p className="text-red-500">
+                {errors.owner_portrait.message as string}
+              </p>
+            )}
+          </div>
+
+          <label htmlFor="owner_portrait">
+            <input
+              type="file"
+              id="owner_portrait"
+              className="hidden"
+              {...register("owner_portrait", {
+                required: "Owner portrait is required",
+              })}
+              onChange={e => {
+                register("owner_portrait").onChange(e);
+              }}
+            />
+
+            <div
+              className={`border rounded-xl w-full py-8 text-center flex flex-col gap-3.5 justify-center items-center cursor-pointer hover:bg-gray-100 duration-300 transition-all ${
+                errors.owner_portrait ? "border-red-500" : "border-[#99a1af8a] "
+              }`}
+            >
+              {ownerPhoto ? (
+                <p className="size-10 rounded-full grid place-items-center bg-green-600  text-white">
+                  <CheckSvg />
+                </p>
+              ) : (
+                <DownloadIconSvg />
+              )}
+              {ownerPhoto ? (
+                <h4 className="text-lg text-gray-500">{ownerPhoto?.name}</h4>
+              ) : (
+                <h4 className="text-lg text-gray-500">Click to upload image</h4>
+              )}
+
+              <p className="text-gray-500 -mt-1">PNG, JPG up to 10MB</p>
+            </div>
+          </label>
+        </div>
+
+        {/* Photos of Your Art / Work (3-5 photos) */}
+        <div>
+          <p className="artist_label mb-1">
+            <ImgSvg />
+            <span>
+              Photos of Your Art / Work (3-5 photos) <span>*</span>
+            </span>
+          </p>
+
+          <div className="flex justify-between items-center mb-3">
+            <p className="text-[#364153]">
+              High-quality photos showcasing your best work. These will appear
+              in your spotlight gallery.
+            </p>
+
+            {errors.workstation?.message && (
+              <p className="text-red-500">
+                {errors.workstation.message as string}
+              </p>
+            )}
+          </div>
+
+          <label htmlFor="workstation">
+            <input
+              type="file"
+              id="workstation"
+              className="hidden"
+              {...register("workstation", {
+                required: "Workspace photo is required",
+              })}
+              onChange={e => {
+                register("workstation").onChange(e);
+              }}
+            />
+
+            <div
+              className={`border rounded-xl w-full py-8 text-center flex flex-col gap-3.5 justify-center items-center cursor-pointer hover:bg-gray-100 duration-300 transition-all ${
+                errors.workstation ? "border-red-500" : "border-[#99a1af8a] "
+              }`}
+            >
+              {workspacePhoto ? (
+                <p className="size-10 rounded-full grid place-items-center bg-green-600  text-white">
+                  <CheckSvg />
+                </p>
+              ) : (
+                <DownloadIconSvg />
+              )}
+              {workspacePhoto ? (
+                <h4 className="text-lg text-gray-500">
+                  {workspacePhoto?.name}
+                </h4>
+              ) : (
+                <h4 className="text-lg text-gray-500">Click to upload image</h4>
+              )}
+
+              <p className="text-gray-500 -mt-1">PNG, JPG up to 10MB</p>
+            </div>
+          </label>
+        </div>
+
+        {/* Product or Service Photos */}
+        <div>
+          <p className="artist_label mb-1">
+            <ImgSvg />
+            <span>
+              Behind-the-Scenes Photo <span>*</span>
+            </span>
+          </p>
+
+          <div className="flex justify-between items-center mb-3">
+            <p className="text-[#364153]">
+              Show your creative process! This adds authenticity to your story.
+            </p>
+
+            {errors.product_photos?.message && (
+              <p className="text-red-500">
+                {errors.product_photos.message as string}
+              </p>
+            )}
+          </div>
+
+          <label htmlFor="product_photos">
+            <input
+              type="file"
+              id="product_photos"
+              className="hidden"
+              {...register("product_photos", {
+                required: "Product photo is required",
+              })}
+              onChange={e => {
+                register("product_photos").onChange(e);
+              }}
+            />
+
+            <div
+              className={`border rounded-xl w-full py-8 text-center flex flex-col gap-3.5 justify-center items-center cursor-pointer hover:bg-gray-100 duration-300 transition-all ${
+                errors.product_photos ? "border-red-500" : "border-[#99a1af8a] "
+              }`}
+            >
+              {servicePhoto ? (
+                <p className="size-10 rounded-full grid place-items-center bg-green-600  text-white">
+                  <CheckSvg />
+                </p>
+              ) : (
+                <DownloadIconSvg />
+              )}
+              {servicePhoto ? (
+                <h4 className="text-lg text-gray-500">{servicePhoto?.name}</h4>
+              ) : (
+                <h4 className="text-lg text-gray-500">Click to upload image</h4>
+              )}
+
+              <p className="text-gray-500 -mt-1">PNG, JPG up to 10MB</p>
+            </div>
+          </label>
+        </div>
+
+        {/* Short Intro Video (15-30 seconds) */}
+        <div>
+          <p className="artist_label mb-1">
+            <ImgSvg />
+            <span>
+              Short Intro Video (15-30 seconds) <span>*</span>
+            </span>
+          </p>
+
+          <div className="flex justify-between items-center mb-3">
+            <p className="text-[#364153]">
+              Introduce yourself on camera! This helps the community connect
+              with you.
+            </p>
+
+            {errors.team_photo?.message && (
+              <p className="text-red-500">
+                {errors.team_photo.message as string}
+              </p>
+            )}
+          </div>
+
+          <label htmlFor="team_photo">
+            <input
+              type="file"
+              id="team_photo"
+              className="hidden"
+              {...register("team_photo", {
+                required: "Team photo is required",
+              })}
+              onChange={e => {
+                register("team_photo").onChange(e);
+              }}
+            />
+
+            <div
+              className={`border rounded-xl w-full py-8 text-center flex flex-col gap-3.5 justify-center items-center cursor-pointer hover:bg-gray-100 duration-300 transition-all ${
+                errors.team_photo ? "border-red-500" : "border-[#99a1af8a] "
+              }`}
+            >
+              {teamPhoto ? (
+                <p className="size-10 rounded-full grid place-items-center bg-green-600  text-white">
+                  <CheckSvg />
+                </p>
+              ) : (
+                <DownloadIconSvg />
+              )}
+              {teamPhoto ? (
+                <h4 className="text-lg text-gray-500">{teamPhoto?.name}</h4>
+              ) : (
+                <h4 className="text-lg text-gray-500">Click to upload image</h4>
+              )}
+
+              <p className="text-gray-500 -mt-1">PNG, JPG up to 10MB</p>
+            </div>
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
