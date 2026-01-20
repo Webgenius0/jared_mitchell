89ecd253861b1e2677768d30d634984bc14402d1
towfiq-islam <tofiqislam03@gmail.com>
2026-01-20 17:34:53 +0600

    Modified nomination page

--- src/app/(main)/nomination/page.tsx
+++ src/app/(main)/nomination/page.tsx
@@ -1,33 +1,33 @@
-"use client"
-
-import { useState } from "react"
-import Container from "@/Components/Common/Container"
-import Voting from "./_components/Voting"
-import OsiPanel from "./_components/OsiPanel"
-import LeaderBoard from "./_components/LeaderBoard"
+"use client";
+import { useState } from "react";
+import Container from "@/Components/Common/Container";
+import Voting from "./_components/Voting";
+import OsiPanel from "./_components/OsiPanel";
+import LeaderBoard from "./_components/LeaderBoard";
 
 const tabs = [
   { id: "1", tab: "Voting" },
   { id: "2", tab: "OSI Panel" },
   { id: "3", tab: "Leader-board" },
-]
+];
 
 const Page = () => {
-  const [activeTab, setActiveTab] = useState("1")
+  const [activeTab, setActiveTab] = useState("1");
 
   return (
     <section>
       <Container>
         {/* Tabs */}
         <div className="w-full border-b border-[#E5E7EB] flex items-center gap-6 my-[40px]">
-          {tabs.map((item) => (
+          {tabs.map(item => (
             <button
               key={item.id}
               onClick={() => setActiveTab(item.id)}
               className={`pb-4 px-4 md:px-6 relative font-medium transition-colors
-                ${activeTab === item.id
+                ${
+                  activeTab === item.id
                     ? "text-[#155DFC] border-b-2 border-[#155DFC]"
-                  : "text-[#4A5565] hover:text-[#578bfa]"
+                    : "text-[#4A5565] hover:text-[#578bfa]"
                 }
               `}
             >
@@ -42,22 +42,14 @@ const Page = () => {
         </div>
 
         {/* Tab Content */}
-        <div className="">
-          {activeTab === "1" && (
-            <Voting />
-          )}
-
-          {activeTab === "2" && (
-            <OsiPanel />
-          )}
-
-          {activeTab === "3" && (
-            <LeaderBoard />
-          )}
+        <div>
+          {activeTab === "1" && <Voting />}
+          {activeTab === "2" && <OsiPanel />}
+          {activeTab === "3" && <LeaderBoard />}
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
