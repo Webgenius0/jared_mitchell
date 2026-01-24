23bc477feee7d5e74801e42c5a7310bfd4fd2254
towfiq-islam <tofiqislam03@gmail.com>
2026-01-24 12:22:17 +0600

    Completed billing page

--- src/app/dashboard/artist_business/billing/page.tsx
+++ src/app/dashboard/artist_business/billing/page.tsx
@@ -1,3 +1,23 @@
+import { HiOutlineDotsVertical } from "react-icons/hi";
+
+const tableData = [
+  { id: "INV-001", date: "Dec1, 2024", amount: "$29.00", status: "paid" },
+  { id: "INV-001", date: "Dec1, 2024", amount: "$29.00", status: "paid" },
+  { id: "INV-001", date: "Dec1, 2024", amount: "$29.00", status: "paid" },
+  { id: "INV-001", date: "Dec1, 2024", amount: "$29.00", status: "paid" },
+];
+
+const data2 = [
+  {
+    title: "50 Support Votes Bundle",
+    time: "Dec 10, 2024",
+  },
+  {
+    title: "50 Support Votes Bundle",
+    time: "Dec 10, 2024",
+  },
+];
+
 const page = () => {
   return (
     <>
@@ -43,6 +63,72 @@ const page = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="card mb-5">
+        <div className="flex gap-3 items-center justify-between mb-3">
+          <h3 className="text-2xl font-medium mb-5">Billing History</h3>
+          <button className="text-sm px-5 py-2 bg-primary-blue rounded-full cursor-pointer text-white font-medium">
+            Export All
+          </button>
+        </div>
+
+        <div className="rounded-lg overflow-hidden">
+          <table className="table-auto w-full">
+            <thead>
+              <tr className="bg-[#E8EFFF]">
+                <td className="p-4 font-medium">Invoice</td>
+                <td className="p-4 font-medium">Date</td>
+                <td className="p-4 font-medium">Amount</td>
+                <td className="p-4 font-medium">Status</td>
+                <td className="p-4 font-medium">Action</td>
+              </tr>
+            </thead>
+
+            <tbody>
+              {tableData?.map(item => (
+                <tr
+                  className="border-b last:border-b-0 border-gray-200 text-gray-700"
+                  key={item?.id}
+                >
+                  <td className="p-4">{item?.id}</td>
+                  <td className="p-4">{item?.date}</td>
+                  <td className="p-4">{item?.amount}</td>
+                  <td className="p-4">
+                    <p className="text-[#1FC16B] bg-[#32A47929] px-4 py-1 text-sm w-fit rounded-full capitalize">
+                      {item?.status}
+                    </p>
+                  </td>
+                  <td className="p-4">
+                    <button>
+                      <HiOutlineDotsVertical className="text-lg" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div className="card mb-5">
+        <h3 className="text-2xl font-medium mb-5">Support Vote Purchases</h3>
+
+        <div className="space-y-4">
+          {data2?.map(item => (
+            <div
+              key={item?.title}
+              className="p-5 rounded-xl bg-[#E8EFFF] flex gap-5 items-center justify-between"
+            >
+              <div className="space-y-1.5">
+                <h3 className="text-lg">{item?.title}</h3>
+                <p className="text-gray-500 text-sm">{item?.time}</p>
+              </div>
+
+              <p className="text-black text-xl font-medium">$25.00</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
