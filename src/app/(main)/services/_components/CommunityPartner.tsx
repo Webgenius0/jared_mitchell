c45287df7ec1c188672cc10edcf462e67c56a73f
towfiq-islam <tofiqislam03@gmail.com>
2026-03-05 10:12:24 +0600

    doen

--- src/app/(main)/services/_components/CommunityPartner.tsx
+++ src/app/(main)/services/_components/CommunityPartner.tsx
@@ -1,36 +1,41 @@
-'use client'
-import { Button } from '@/Components/Common/Button'
-import Container from '@/Components/Common/Container'
-import SponsorSlider from '@/Components/Common/SponsorSlider'
-import { sponsorsData } from '@/Components/Data/data'
-import { HandShakeSvg } from '@/Components/Svg/SvgContainer'
-import { BsArrowRight } from 'react-icons/bs'
+"use client";
+import { Button } from "@/Components/Common/Button";
+import SponsorSlider from "@/Components/Common/SponsorSlider";
+import { sponsorsData } from "@/Components/Data/data";
+import { HandShakeSvg } from "@/Components/Svg/SvgContainer";
+import { BsArrowRight } from "react-icons/bs";
 
 const CommunityPartner = () => {
-    return (
-        <section className='section'>
-            <Container>
-                <div className='text-center'>
-                <div className='text-primary-blue bg-[rgba(25,119,221,0.16)] inline-flex items-center gap-2.5 px-5 py-2.5 rounded-full'>
-                    <HandShakeSvg />
-                    Community Partners
-                </div>
-                <h2 className='section_title 2xl:text-7xl 2xl:font-bold'>Our Members Grow With the Support of Our Partners and Sponsors</h2>
-                <p className='section_sub_title'>We collaborate with a growing network of small businesses, community leaders, creative professionals, and brands committed to uplifting culture, entrepreneurship, and artistic expression.</p>
-                </div>
-            </Container>
-            <div className='flex flex-col gap-y-2 mt-6 md:mt-10'>
-                <SponsorSlider logos={sponsorsData} />
-                <SponsorSlider logos={sponsorsData} reverse={true} />
-            </div>
-            <div className='text-center mt-8 space-x-4'>
-                <Button>
-                    Become a Sponsor
-                    <BsArrowRight className='text-2xl' />
-                </Button>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="text-center">
+          <div className="text-primary-blue bg-[rgba(25,119,221,0.16)] inline-flex items-center gap-2.5 px-5 py-2.5 rounded-full mb-3">
+            <HandShakeSvg />
+            Community Partners
+          </div>
+          <h2 className="section_title 2xl:text-7xl 2xl:font-bold">
+            Our Members Grow With the Support of Our Partners and Sponsors
+          </h2>
+          <p className="section_sub_title">
+            We collaborate with a growing network of small businesses, community
+            leaders, creative professionals, and brands committed to uplifting
+            culture, entrepreneurship, and artistic expression.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-2 mt-6 md:mt-10">
+        <SponsorSlider logos={sponsorsData} />
+        <SponsorSlider logos={sponsorsData} reverse={true} />
+      </div>
+      <div className="text-center mt-8 space-x-4">
+        <Button>
+          Become a Sponsor
+          <BsArrowRight className="text-2xl" />
+        </Button>
+      </div>
+    </section>
+  );
+};
 
-export default CommunityPartner
+export default CommunityPartner;
--- src/app/(main)/services/_components/ServiceBanner.tsx
+++ src/app/(main)/services/_components/ServiceBanner.tsx
@@ -11,10 +11,10 @@ const ServiceBanner = () => {
           url(${serviceBg.src})
         `,
       }}
-      className="h-[500px] bg-no-repeat bg-center bg-cover"
+      className="h-[300px] md:h-[400px] xl:h-[500px] bg-no-repeat bg-center bg-cover"
     >
       <div className="h-full container flex flex-col items-center justify-center">
-        <figure className="w-[238px] h-[161px]">
+        <figure className="w-[150px] md:w-[200px] xl:w-[238px] xl:h-[161px]">
           <Image
             src={about_us_logo}
             alt="about_us_logo"
@@ -23,11 +23,11 @@ const ServiceBanner = () => {
           />
         </figure>
 
-        <h2 className="text-5xl font-semibold leading-[150%] text-white text-center max-w-[968px] mx-auto">
+        <h2 className="text-[22px] md:text-5xl font-semibold md:leading-[150%] text-white text-center max-w-[968px] mx-auto">
           Empower your growth with the tools, visibility, and AI insights of
         </h2>
 
-        <p className="text-[#5E90FF] font-bold text-4xl pt-5">
+        <p className="text-[#5E90FF] text-center md:text-xl xl:text-[26px] pt-3 md:pt-5">
           Our Social Image
         </p>
       </div>
