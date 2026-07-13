670d80fb6848615760c6d1de9212149f746d279a
fahim-sarker <sarkerfahim599@gmail.com>
2026-07-13 10:51:21 +0600

    refined: refactor the home page

--- src/app/(main)/events/[slug]/page.tsx
+++ src/app/(main)/events/[slug]/page.tsx
@@ -3,13 +3,14 @@ import { use, useEffect, useState, useContext } from "react";
 import { getEventBySlug } from "@/lib/Services/cms_service";
 import { CMSEventItem } from "@/Types/cms";
 import { AuthContextProvider } from "@/Provider/AuthProvider/AuthProvider";
+import { PageLoader } from "@/Shared/PageLoader";
 
 export default function page({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const { slug } = use(params); 
+  const { slug } = use(params);
   const { token } = useContext(AuthContextProvider);
   const [eventDetails, setEventDetails] = useState<CMSEventItem | null>(null);
 
@@ -19,9 +20,16 @@ export default function page({
     }
   }, [slug, token]);
 
-  if (!eventDetails) return <div>Loading...</div>;
+  if (!eventDetails)
+    return (
+      <div className="">
+        <PageLoader />
+      </div>
+    );
 
   return (
-    <div>{eventDetails?.title}</div>
+    <section className="lg:py-25 py-15">
+      <div className="container mx-auto"></div>
+    </section>
   );
 }
