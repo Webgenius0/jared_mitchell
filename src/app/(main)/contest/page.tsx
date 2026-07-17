111429c5cdd8a94c45ff0de0f699a186fd6e4885
fahim-sarker <sarkerfahim599@gmail.com>
2026-07-17 15:38:13 +0600

    feat: added the contest page

--- src/app/(main)/contest/page.tsx
+++ src/app/(main)/contest/page.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { CMSBossBeginnings } from "@/Types/cms";
 import ContestBanner from "./Components/ContestBanner";
-import { getBossCms } from "@/lib/Services/cms_service";
+import { getBossCms, getCMSAboutData } from "@/lib/Services/cms_service";
 import ContestSpotlights from "./Components/ContestSpotlights";
 import ContestTable from "./Components/ContestTable";
+import Sponsors from "../_components/Sponsors";
+import NewsLetter from "@/Components/Common/NewsLetter";
 
 const page = async () => {
   const pageData = (await getBossCms()) as CMSBossBeginnings;
+  const CmsData = await getCMSAboutData();
+
   return (
     <div>
       <ContestBanner data={pageData?.boss_beginnings_hero} />
       <ContestSpotlights />
       <ContestTable />
+      <Sponsors data={CmsData?.about_sponsors} />
+      <NewsLetter title="Be part of the movement. Get stories, updates, and opportunities straight to your inbox." />
     </div>
   );
 };
