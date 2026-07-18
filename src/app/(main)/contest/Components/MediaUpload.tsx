bc0bd60cc70639f3185e625dea13e42660e6c67c
fahim-sarker <sarkerfahim599@gmail.com>
2026-07-18 08:55:01 +0600

    feat: added the mediauploads  section

---
+++ src/app/(main)/contest/Components/MediaUpload.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+import Media1 from "../../../../Assets/Rectangle 4437 (1).png";
+import Media2 from "../../../../Assets/Rectangle 4437.png";
+import Media3 from "../../../../Assets/Rectangle 4438.png";
+
+export default function MediaUpload() {
+  return (
+    <section className="py-20">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-6">
+          {[Media1, Media2, Media3, Media2, Media1, Media3].map(
+            (image, index) => (
+              <div key={index} className="overflow-hidden rounded-[16px]">
+                <Image
+                  src={image}
+                  alt={`Media ${index + 1}`}
+                  width={500}
+                  height={300}
+                  className="h-full w-full object-cover transition-transform duration-300 ease-in-out hover:scale-110"
+                />
+              </div>
+            ),
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/(main)/contest/[id]/page.tsx
+++ src/app/(main)/contest/[id]/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SpotlightDetails from "../Components/SpotlightDetails";
 import ArtistStory from "../Components/ArtistStory";
+import MediaUpload from "../Components/MediaUpload";
 
 export default function page() {
   return (
     <>
       <SpotlightDetails />
       <ArtistStory />
+      <MediaUpload />
     </>
   );
 }
