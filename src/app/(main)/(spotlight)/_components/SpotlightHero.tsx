93786d7264f866424933e2e9cc9249ba16d89ad8
fahim-sarker <sarkerfahim599@gmail.com>
2026-07-28 16:44:56 +0600

    fix: fixed the spotlighhero

--- src/app/(main)/(spotlight)/_components/SpotlightHero.tsx
+++ src/app/(main)/(spotlight)/_components/SpotlightHero.tsx
@@ -1,16 +1,16 @@
-import { Button } from '@/Components/Common/Button'
-import Container from '@/Components/Common/Container'
-import CustomVideoPlayer from '@/Components/Common/CustomVideoPlayer'
-import { CMSArtistSpotlightVideo } from '@/Types/cms'
+import { Button } from "@/Components/Common/Button";
+import Container from "@/Components/Common/Container";
+import CustomVideoPlayer from "@/Components/Common/CustomVideoPlayer";
+import { CMSArtistSpotlightVideo } from "@/Types/cms";
 
 const SpotlightHero = ({ data }: { data?: CMSArtistSpotlightVideo }) => {
   return (
-    <section className='section'>
-      <Container >
-        <div className='w-full h-[300px] md:h-[500px] xl:h-[627px]'>
-          <CustomVideoPlayer 
-            videoSrc={data?.description || "/home/hero-video.mp4"} 
-            className={"!rounded-[20px] md:!rounded-[40px]"} 
+    <section className="section">
+      <Container>
+        <div className="w-full h-[300px] md:h-[500px] xl:h-[627px]">
+          <CustomVideoPlayer
+            videoSrc={data?.video || "/home/hero-video.mp4"}
+            className={"!rounded-[20px] md:!rounded-[40px]"}
           />
         </div>
         {/* <h2 className='text-2xl md:text-3xl xl:text-[40px] font-bold text-center mt-8 md:mt-16'>
@@ -23,7 +23,7 @@ const SpotlightHero = ({ data }: { data?: CMSArtistSpotlightVideo }) => {
         )}
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default SpotlightHero
+export default SpotlightHero;
