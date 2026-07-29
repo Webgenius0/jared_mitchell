078f06d4e24fc812bb68aa814db201505fc54507
fahim-sarker <sarkerfahim599@gmail.com>
2026-07-29 11:07:58 +0600

    feat: intriget business dashboard api

--- src/Hooks/api/dashboard_api.ts
+++ src/Hooks/api/dashboard_api.ts
@@ -1,1 +1,11 @@
-// All Dashboard API
+import useClientApi from "../useClientApi";
+
+// Get Boss Beginning Dashboard Stats
+export const useGetDashboardStats = () => {
+  return useClientApi({
+    method: "get",
+    isPrivate: true,
+    key: ["boss-dashboard-stats"],
+    endpoint: "/v1/dashboard/stats",
+  });
+};
